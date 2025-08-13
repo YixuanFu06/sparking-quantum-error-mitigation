922001854a95fcbb3aafcced3dca78c2873c6fb5
Yuxinyuan2009 <1993133029@qq.com>
2025-08-13 06:22:57 +0000

    add the result of noisy case of group 3

---
+++ question3/ZNE_Ising_model.py
@@ -0,0 +1,79 @@
+import numpy as np
+import tensorcircuit as tc
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from noise_cz import expectation_n_noisy_channel_cz
+
+K = tc.set_backend("tensorflow")
+
+w = 0.9999 * np.ones(16)
+
+T = 10
+
+
+def cnot(circuit, control, target, x):
+    circuit.H(target)
+    expectation_n_noisy_channel_cz(circuit, control, target, w, x)
+    circuit.H(target)
+
+
+
+
+def rzz (c, i, j, theta, x):
+    #c.cnot(j, i)
+    cnot(c, j, i, x)
+    c.rz(i, theta=theta)
+    #c.cnot(j, i)
+    cnot(c, j, i, x)
+    return c
+
+def P (c, n, edges, N, J = -1., h = 1., x = 0.):
+    for i in range(n):
+        c.rx(i, theta = 2*T*h/N)
+    for g0,g1 in edges:
+        c = rzz(c, g0, g1, -2*T*J/N, x)
+    return c
+
+def test (c, n, edge, N, J = -1., h = 1., x = 0.):
+    #c=tc.Circuit(n)
+    for i in range(N):
+        c = P(c, n, edge, N, J, h, x)
+    return c
+    # z_exp = c.expectation([tc.gates.z(), [0]])
+    # z_exp_float = float(z_exp.numpy())
+
+    # return z_exp_float
+
+n = 5
+edges = [[1, 2], [3, 4], [0, 1], [2, 3], [1, 2], [3, 4]]
+
+# n = 6
+# edges = [[0, 1], [3, 4], [2, 5], [0, 3], [4, 5], [1, 2], [1, 4]]
+
+# n = 9
+# edges = [[0, 1], [3, 4], [7, 8], [2, 5], [0, 3], [4, 5], [6, 7], [1, 2], [4, 7], [5, 8], [3, 6], [1, 4]]
+
+
+
+e = 0
+f = np.array([0., 3., -3., 1., 0., 0., 0., 0., 0., 0.], dtype=np.complex128)
+g = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8], dtype=np.int32)
+
+r = np.array([0., 0., 0., 0., 0.], dtype=np.complex128)
+
+
+
+for p in range(5, 9):
+    t = g[p]
+    a = 0
+    for i in range(300):
+        c = tc.Circuit(n)
+        N = 20
+        c = test(c, n, edges, N, 1, 1, t)
+        a += c.expectation_ps(z=[0, 1, 2, 3, 4])
+    a /= 300
+    print(g[p], ":", a)
+    a *= f[p]
+    e += a
+
+print(e)
--- question3/p.py
+++ question3/p.py
@@ -7,9 +7,9 @@ plt.rcParams['font.size'] = 20                  # 设置全局字号
 plt.rcParams['mathtext.fontset'] = 'stix'       # 数学公式字体（与Times New Roman风格一致）
 
 # 你的数据
-n_noise_list_int = np.array([1, 2, 3, 4])
+n_noise_list_int = np.array([1, 2, 3, 4, 5, 6])
 x_values = np.array(n_noise_list_int)
-y_values = np.real(np.array([0.022222, 0.012770, 0.002710, 0.006397]))
+y_values = np.real(np.array([0.05816, 0.02586, 0.02970, 0.01782, 0.01866, 0.00079]))
 
 # 定义多项式模型
 def poly_func(x, a, b):
@@ -25,20 +25,20 @@ y_fit = poly_func(x_fit, *popt)
 plt.figure(figsize=(10, 6))
 
 # 添加标题和标签（字号自动继承全局设置）
-plt.title('ZNE for Ising model(n=5, 200 times, w_k=0.9999)')
+plt.title('ZNE for Ising model(n=5, 300 times, w_k=0.9999)')
 plt.xlabel('Number of Noisy Layer')
 plt.ylabel('Expectation <Z01234>')
 
 # 设置坐标轴范围和刻度
 # plt.xlim(-0.5, max(x_values) + 0.5)
-plt.ylim(0.00, 0.04)
+# plt.ylim(0.00, 0.04)
 # plt.xticks([0, 4, 8, 12, 16, 20])  # 手动设置x轴刻度
 
 # 添加网格
 plt.grid(True, linestyle='--', alpha=0.7)
 
 # 绘制数据点、拟合曲线和理论值
-theor_exp = 0.025914
+theor_exp = 0.08214
 plt.scatter(0, theor_exp, color='red', s=100, label='Theoretical expectation (no noise)')
 plt.scatter(0, poly_func(0, *popt), color='blue', s=100, label='Fitted expectation (no noise)')
 plt.scatter(x_values, y_values, facecolors='none', edgecolors='blue', s=100, label='Measured Data Points')
