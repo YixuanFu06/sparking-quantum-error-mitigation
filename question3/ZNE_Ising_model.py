2ce97221af729864fb7bc75fc04043820dd2946a
Xinyuan Yu <1993133029@qq.com>
2025-08-15 15:34:34 +0800

    finishing stage

--- question3/ZNE_Ising_model.py
+++ question3/ZNE_Ising_model.py
@@ -3,10 +3,11 @@ import tensorcircuit as tc
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from noise_cz import expectation_n_noisy_channel_cz
+from noise_sim_tc import noise_sim
 
 K = tc.set_backend("tensorflow")
 
-w = 0.9999 * np.ones(16)
+w = 0.9999*np.ones(16)
 
 T = 10
 
@@ -44,11 +45,11 @@ def test (c, n, edge, N, J = -1., h = 1., x = 0.):
 
     # return z_exp_float
 
-n = 5
-edges = [[1, 2], [3, 4], [0, 1], [2, 3], [1, 2], [3, 4]]
+# n = 5
+# edges = [[1, 2], [3, 4], [0, 1], [2, 3], [1, 2], [3, 4]]
 
-# n = 6
-# edges = [[0, 1], [3, 4], [2, 5], [0, 3], [4, 5], [1, 2], [1, 4]]
+n = 6
+edges = [[0, 1], [3, 4], [2, 5], [0, 3], [4, 5], [1, 2], [1, 4]]
 
 # n = 9
 # edges = [[0, 1], [3, 4], [7, 8], [2, 5], [0, 3], [4, 5], [6, 7], [1, 2], [4, 7], [5, 8], [3, 6], [1, 4]]
@@ -63,15 +64,17 @@ r = np.array([0., 0., 0., 0., 0.], dtype=np.complex128)
 
 
 
-for p in range(5, 9):
+for p in range(1):
     t = g[p]
     a = 0
-    for i in range(300):
-        c = tc.Circuit(n)
+    for i in range(1):
+        c = tc.densitymatrix.DMCircuit(n)
         N = 20
         c = test(c, n, edges, N, 1, 1, t)
-        a += c.expectation_ps(z=[0, 1, 2, 3, 4])
-    a /= 300
+        #c = noise_sim(c)
+        a += c.expectation_ps(z=[0])
+        #print(c.expectation_ps(z=[0]))
+    a /= 1
     print(g[p], ":", a)
     a *= f[p]
     e += a
--- question3/plotting.py
+++ question3/plotting.py
@@ -7,17 +7,17 @@ plt.rcParams['font.size'] = 20                  # 设置全局字号
 plt.rcParams['mathtext.fontset'] = 'stix'       # 数学公式字体（与Times New Roman风格一致）
 
 # 你的数据
-n_noise_list_int = np.array([1, 2, 3, 4, 5, 6])
+n_noise_list_int = np.array([1, 2, 3, 4])
 x_values = np.array(n_noise_list_int)
-y_values = np.real(np.array([0.05816, 0.02586, 0.02970, 0.01782, 0.01866, 0.00079]))
+y_values = np.real(np.array([0.13280, 0.07428, 0.03862, 0.02536]))
 
 # 定义多项式模型
-def poly_func(x, a, b):
-    return a * x + b
+def poly_func(x, a, b, c):
+    return a * x**2 + b * x + c
 
 
 # 拟合
-popt, pcov = curve_fit(poly_func, x_values, y_values, p0=(1, -0.1))
+popt, pcov = curve_fit(poly_func, x_values, y_values, p0=(1, -0.1, 0))
 x_fit = np.linspace(-0.5, max(x_values), 100)
 y_fit = poly_func(x_fit, *popt)
 
@@ -25,9 +25,9 @@ y_fit = poly_func(x_fit, *popt)
 plt.figure(figsize=(10, 6))
 
 # 添加标题和标签（字号自动继承全局设置）
-plt.title('ZNE for Ising model(n=5, 300 times, w_k=0.9999)')
+plt.title('ZNE for Ising model(n=9, 500 times)')
 plt.xlabel('Number of Noisy Layer')
-plt.ylabel('Expectation <Z01234>')
+plt.ylabel('Expectation <Z0>')
 
 # 设置坐标轴范围和刻度
 # plt.xlim(-0.5, max(x_values) + 0.5)
@@ -38,7 +38,7 @@ plt.ylabel('Expectation <Z01234>')
 plt.grid(True, linestyle='--', alpha=0.7)
 
 # 绘制数据点、拟合曲线和理论值
-theor_exp = 0.08214
+theor_exp = 0.21607
 plt.scatter(0, theor_exp, color='red', s=100, label='Theoretical expectation (no noise)')
 plt.scatter(0, poly_func(0, *popt), color='blue', s=100, label='Fitted expectation (no noise)')
 plt.scatter(x_values, y_values, facecolors='none', edgecolors='blue', s=100, label='Measured Data Points')
